a6585b651200ac2ecf845fb65292aae78657a760
Mofeed25 <mueedabd92@gmail.com>
2026-06-30 23:59:04 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import "./globals.css"; // 👈 استدعاء التنسيقات هنا يضمن تفعيل Tailwind CSS في كامل الموقع
+import "./styles/globals.css";
 
 export default function RootLayout({ children }: any) {
     return (
         <html lang="ar" dir="rtl">
             <body className="bg-[#0B192C] text-white">
-                {/* تم تنظيف الـ Layout من الـ Sidebar ليظهر فقط بالصفحات المحمية */}
                 {children}
             </body>
         </html>
