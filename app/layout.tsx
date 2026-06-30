dac851388fb807cd1e1ae7555ae09435c46f6499
Mofeed25 <mueedabd92@gmail.com>
2026-07-01 00:05:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./styles/globals.css";
+import "../styles/globals.css"; // 👈 الخروج خطوة للأعلى للوصول لمجلد styles الموجود في جذر المشروع
 
 export default function RootLayout({ children }: any) {
     return (
